83a12e386c1ba7f2eb48a51257c614ebdb93f30e
Perez <gabrieljesusperez@hotmail.com>
2021-04-07 17:55:27 -0300

    Inserção classes de serviços , marketplace, master, report, split

---
+++ src/gabrieljperez/Iugu/Iugu.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Iugu;
+
+use bubbstore\Iugu\Iugu as baseIugu;
+
+use App\Iugu\Marketplace;
+use App\Iugu\Master;
+use \App\Iugu\Split;
+
+/**
+ * Class Iugu.
+ *
+ * @package namespace App\Iugu;
+ */
+class Iugu extends baseIugu
+{
+    /**
+     * Marketplace
+     *
+     * @var \App\Iugu\MarketPlace
+     */
+    protected $Marketplace;
+
+    /**
+     * Master
+     * 
+     * @var \App\Iugu\Master
+     */
+    protected $Master;
+
+    /**
+     * Split
+     * 
+     * @var \App\Iugu\Split
+     */
+    protected $Split;
+
+    public function __construct()
+    {
+        parent::__construct($this->_getApiKey());
+
+        $this->Marketplace = new Marketplace($this->http, $this);
+        $this->Master = new Master($this->http, $this);
+        $this->Split = new Split($this->http, $this);
+    }
+
+    /**
+     * Get api key
+     * 
+     * @return string
+     */
+    private function _getApiKey()
+    {
+        if (config('app.debug')) {
+            $key = config('services.iugu.api_test_key');
+        } else {
+            $key = config('services.iugu.api_key');
+        }
+
+        return $key;
+    }
+
+    /**
+     * Get account Id
+     * 
+     * @return string
+     */
+    public function getAccountId()
+    {
+        return config('services.iugu.account_id');
+    }
+
+    /**
+     * Marketplace
+     * 
+     * @return App\Iugu\Marketplace 
+     */
+    public function marketplace()
+    {
+        return $this->Marketplace;
+    }
+
+    /**
+     * Master account settings
+     *
+     * @return App\Iugu\Master
+     */
+    public function master()
+    {
+        return $this->Master;
+    }
+
+    /**
+     * Split
+     *
+     * @return App\Iugu\Split
+     */
+    public function Split()
+    {
+        return $this->Split;
+    }
+}
---
+++ src/gabrieljperez/Iugu/Marketplace.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Iugu;
+
+use bubbstore\Iugu\Services\BaseRequest;
+
+/**
+ * Class Marketplace.
+ *
+ * @package namespace App\Iugu;
+ */
+class Marketplace extends BaseRequest
+{
+    public function __construct($http, $iugu)
+    {
+        parent::__construct($http, $iugu);
+    }
+
+    /**
+     * Listar subcontas
+     * 
+     * Lista as contas de um marketplace ou parceiro de negócios
+     *
+     * @return void
+     */
+    public function list()
+    {
+        $this->sendApiRequest('GET', 'marketplace');
+
+        return $this->fetchResponse();
+    }
+
+    /**
+     * create
+     *
+     * Cria um novo marketplace.
+     *
+     * @param array $params
+     * @return array
+     */
+    public function create(array $params)
+    {
+        $this->setParams($params)->sendApiRequest('POST', 'marketplace/create_account');
+
+        return $this->fetchResponse();
+    }
+
+    /**
+     * Update
+     *
+     * Atualiza uma subconta.
+     *
+     * @param  int $id
+     * @param  array $params 
+     * @return array
+     */
+    public function update($id, array $params)
+    {
+        $this->setParams($params)->sendApiRequest('PUT', sprintf('accounts/$s', $id));
+
+        return $this->fetchResponse();
+    }
+
+    /**
+     * Envia uma verificação de subconta
+     *
+     * @param  int $id
+     * @param  array $params 
+     * @return array
+     */
+    public function requestVerification($id, array $params)
+    {
+        $this->setParams($params)->sendApiRequest('POST', sprintf('accounts/%s/request_verification', $id));
+
+        return $this->fetchResponse();
+    }
+
+    /**
+     * Show
+     *
+     * Retorna os dados da subconta
+     *
+     * @param  int $id
+     * @return array
+     */
+    public function show($id)
+    {
+        $this->sendApiRequest('GET', sprintf('accounts/%s', $id));
+
+        return $this->fetchResponse();
+    }
+
+    /**
+     * requestWithdraw
+     *
+     * Faz um pedido de Saque de um valor.
+     *
+     * @param  int $id 
+     * @param  array $params 
+     * @return array
+     */
+    public function requestWithdraw($id, array $params)
+    {
+        $this->sendApiRequest('POST', sprintf('accounts/%s/request_withdraw', $id));
+
+        return $this->fetchResponse();
+    }
+}
